fbe69b83b061c9ff865276fca1e777790d1dfed5
Thomas Thornton-Rasmussen <thomas@thornton.dk>
2021-06-19 19:51:00 +0200

    populate inventory

--- PromotionEngine/Program.cs
+++ PromotionEngine/Program.cs
@@ -7,7 +7,12 @@ namespace PromotionEngine
     {
         static void Main(string[] args)
         {
-            
+            List<SKU> inventory = new List<SKU>();
+
+            inventory.Add(new SKU("A", 50));
+            inventory.Add(new SKU("B", 30));
+            inventory.Add(new SKU("C", 20));
+            inventory.Add(new SKU("D", 15));
         }
     }
 }
