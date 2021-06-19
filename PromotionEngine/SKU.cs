224224119445320f92d2e1ba96a3d4ef9230a5a9
Thomas Thornton-Rasmussen <thomas@thornton.dk>
2021-06-19 19:39:00 +0200

    SKU class added

---
+++ PromotionEngine/SKU.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    class SKU
+    {
+        public string ID { get; }
+        public float Price { get; }
+
+        public SKU(string id, float price)
+        {
+            this.ID = id;
+            this.Price = price;
+        }
+    }
+}
